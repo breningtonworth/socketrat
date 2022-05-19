df641e2b0d038412a3f7d3c56fbc5593cd2c4165
brenw0rth <brenworth0@gmail.com>
2022-05-19 18:37:54 +0000

    remove requirements

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from setuptools import setup
 
 setup(
         name='socketrat',
-        version='0.1',
+        version='0.1.0',
         packages=['socketrat'],
         #entry_points="""
         #[console_scripts]
         #socketrat = socketrat:main
         #""",
         install_requires=[
-            'colorama>=0.4.4',
-            'tabulate>=0.8.9',
-            'tqdm>=4.62.0',
             'pyreadline>=2.1',
         ],
 )
