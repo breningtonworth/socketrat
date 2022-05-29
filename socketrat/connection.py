af5ceafcad47838d24386a8557c361e4b53acd47
brenw0rth <brenworth0@gmail.com>
2022-05-29 18:57:03 +0000

    refactor connection class and fix memory error

--- socketrat/connection.py
+++ socketrat/connection.py
@@ -7,6 +7,7 @@ class Connection:
 
     def __init__(self, sock):
         self._sock = sock
+        self._header_struct = struct.Struct('!I')
         self.host, self.port = sock.getpeername()
 
     def __enter__(self):
@@ -15,26 +16,34 @@ class Connection:
     def __exit__(self, *args, **kwargs):
         self.close()
 
-    def send(self, msg):
-        msg = struct.pack('>I', len(msg)) + msg
-        self._sock.sendall(msg)
-
-    def recv(self):
-        raw_msglen = self._recvall(4)
-        msglen = struct.unpack('>I', raw_msglen)[0]
-        return self._recvall(msglen)
-
-    def _recvall(self, n):
-        data = b''
-        while len(data) < n:
-            packet = self._sock.recv(n - len(data))
-            if not packet:
-                raise ConnectionClosed
-            data += packet
-        return data
-
     def close(self):
         self._sock.close()
+
+    def send(self, message):
+        message = self._header_struct.pack(len(message)) + message
+        self._sock.sendall(message)
+
+    def recv(self):
+        data = self._recvall(self._header_struct.size)
+        (block_length,) = self._header_struct.unpack(data)
+        #print('SOCK: {}, LENGTH: {}'.format(id(self._sock), block_length))
+        if block_length > 4096:
+            # A large recv length can raise a MemoryError.
+            # Close connection on anything > 4096.
+            #print('POTENTIAL MEMORY ERROR, CLOSING CONNECTION.')
+            self.close()
+            raise ConnectionClosed
+        return self._recvall(block_length)
+
+    def _recvall(self, length):
+        blocks = list()
+        while length:
+            block = self._sock.recv(length)
+            if not block:
+                raise ConnectionClosed
+            length -= len(block)
+            blocks.append(block)
+        return b''.join(blocks)
 
 
 class ConnectionClosed(Exception):
